28d8ecd597ab9478be9824e84042cd9277bf6e9f
Styro <69657780+Styro457@users.noreply.github.com>
2025-10-22 00:26:05 +0300

    Implement operators for Pos2

--- src/engine/utils/math/pos2.rs
+++ src/engine/utils/math/pos2.rs
@@ -5,7 +5,7 @@ pub struct Pos2 {
     pub y: u32,
 }
 
-//TODO: Implement operators and other methods for Vec2
+//TODO: Implement operators and other methods for Pos2
 impl Pos2 {
     pub const ZERO: Self = Self { x: 0, y: 0 };
 
@@ -19,4 +19,57 @@ impl From<(u32, u32)> for Pos2 {
         Pos2 { x: tuple.0, y: tuple.1 }
     }
 }
+
+// Implement operators
+impl std::ops::Div for Pos2 {
+    type Output = Self;
+    fn div(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x / rhs.x,
+            y: self.y / rhs.y,
+        }
+    }
+}
+
+impl std::ops::Mul for Pos2 {
+    type Output = Self;
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+        }
+    }
+}
+
+impl std::ops::Add for Pos2 {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl std::ops::Sub for Pos2 {
+    type Output = Self;
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+        }
+    }
+}
+
+impl std::ops::Add<u32> for Pos2 {
+    type Output = Self;
+    fn add(self, rhs: u32) -> Self::Output {
+        Self {
+            x: self.x + rhs,
+            y: self.y + rhs,
+        }
+    }
+}
+
+
 
