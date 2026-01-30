2a963c7d1e9ae642c1d7c662ab069a6176a6f3e7
Styro <69657780+Styro457@users.noreply.github.com>
2026-01-30 18:21:31 +0200

    Make SolidPainter paint tiles properly based on size

--- src/engine/painters/solid.rs
+++ src/engine/painters/solid.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use crate::engine::document::Document;
 use crate::engine::image::color::Color;
 use crate::engine::image::tiles::grid::TileGrid;
@@ -16,7 +17,7 @@ pub struct SolidPainter {
 impl SolidPainter {
 
     pub fn new(document: &mut Document, size: Pos2, color: Color) -> Self {
-        let tile_grid = document.tile_manager.create_tile_grid(size);
+        let tile_grid = document.tile_manager.create_tile_grid(document.size);
         let mut obj = Self {
             size,
             color,
@@ -26,18 +27,36 @@ impl SolidPainter {
         obj
     }
 
+    // Not optimized implementation since this Painter is only intended for testing
     pub fn set_color(&mut self, color: Color, document: &mut Document) {
         self.color = color;
+
+        let tile_size = document.tile_manager.get_tile_size();
+
+        let tiles_x = (self.size.x + tile_size.x - 1) / tile_size.x;
+        let tiles_y = (self.size.y + tile_size.y - 1) / tile_size.y;
+
         let tile_grid = &mut self.tile_grid;
-        for i in 0..tile_grid.size.x {
-            for j in 0..tile_grid.size.y {
-                let mut tile: Tile = document.tile_manager.create_tile(document);
-                for x in 0..tile.buffer.len() {
-                    tile.buffer.set_pixel(x, self.color);
+
+        for i in 0..tiles_x {
+            for j in 0..tiles_y {
+                let tile = document.tile_manager.get_tile_or_create(tile_grid, i, j);
+
+                let tile_start_x = i * tile_size.x;
+                let tile_start_y = j * tile_size.y;
+
+                let valid_width = min(tile_size.x, self.size.x.saturating_sub(tile_start_x));
+                let valid_height = min(tile_size.y, self.size.y.saturating_sub(tile_start_y));
+
+                for y in 0..valid_width {
+                    for x in 0..valid_height {
+                        let index = (y * tile_size.x) + x;
+                        tile.set_pixel(index as usize, self.color);
+                    }
                 }
-                document.tile_manager.add_tile(tile_grid, tile, Pos2::new(i, j));
             }
         }
+        tile_grid.dirty = true;
     }
 }
 
