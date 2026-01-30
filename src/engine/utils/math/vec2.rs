0428b72bf31aa91d52e738016ca29278d999527a
Styro <69657780+Styro457@users.noreply.github.com>
2026-01-30 19:42:02 +0200

    Implement scalar operations for Vec2

--- src/engine/utils/math/vec2.rs
+++ src/engine/utils/math/vec2.rs
@@ -70,12 +70,42 @@ impl std::ops::Sub for Vec2 {
     }
 }
 
-impl std::ops::Add<u32> for Vec2 {
+impl std::ops::Add<f32> for Vec2 {
     type Output = Self;
-    fn add(self, rhs: u32) -> Self::Output {
+    fn add(self, rhs: f32) -> Self::Output {
         Self {
             x: self.x + rhs,
             y: self.y + rhs,
+        }
+    }
+}
+
+impl std::ops::Sub<f32> for Vec2 {
+    type Output = Self;
+    fn sub(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x - rhs,
+            y: self.y - rhs,
+        }
+    }
+}
+
+impl std::ops::Div<f32> for Vec2 {
+    type Output = Self;
+    fn div(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x / rhs,
+            y: self.y / rhs,
+        }
+    }
+}
+
+impl std::ops::Mul<f32> for Vec2 {
+    type Output = Self;
+    fn mul(self, rhs: f32) -> Self::Output {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
         }
     }
 }
